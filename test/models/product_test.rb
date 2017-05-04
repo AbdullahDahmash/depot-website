4bc857e858fe7f1b228538d3013ef3f619904298
Abdullah <Abdullah.dahmash@yahoo.com>
2017-05-04 16:08:42 +0300

    product model unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,4 +9,56 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
     assert product.errors[:price].any?
   end
+
+  test "product price must be positive" do
+    product = Product.new title: "My Book Title",
+        description: "ddd",
+        image_url: "zzz.png"
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new title: "My Book Title",
+      description: "ddd",
+      price: 1,
+      image_url: image_url
+  end
+
+  test "image url" do
+    ok = %w(
+      fred.gif
+      fred.jpg
+      fred.png
+      FRED.JPG
+      FRED.Jpg 
+      http://a.b.c/x/y/z/fred.gif
+    )
+
+    bad = %w(
+      fred.doc
+      fred.gif/more
+      fred.gif.more
+    )
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+
+  end
 end
