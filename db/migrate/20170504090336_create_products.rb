aeb08d9583e18af19c30c837cea4343ca5adceb1
Abdullah <Abdullah.dahmash@yahoo.com>
2017-05-04 12:05:26 +0300

    changed description to text type

--- db/migrate/20170504090336_create_products.rb
+++ db/migrate/20170504090336_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.string :image_url
       t.decimal :price, precision: 8, scale: 2
 
