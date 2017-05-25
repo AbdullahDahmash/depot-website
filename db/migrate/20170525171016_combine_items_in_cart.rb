b408c03da00a3ec7a3c4178a3f066c8b851310e0
Abdullah <Abdullah.dahmash@yahoo.com>
2017-05-25 21:05:20 +0300

    combine items in cart "down"

--- db/migrate/20170525171016_combine_items_in_cart.rb
+++ db/migrate/20170525171016_combine_items_in_cart.rb
@@ -12,4 +12,17 @@ class CombineItemsInCart < ActiveRecord::Migration[5.1]
       end
     end
   end
+
+  def down
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
+        )
+      end
+      line_item.destroy
+    end
+  end
 end
