cdb5b77df09e5c0e17c91df8ebe1346fdbb32d2b
Abdullah <Abdullah.dahmash@yahoo.com>
2017-05-25 19:54:12 +0300

    counter visits

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,6 +26,7 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
+    session.delete :counter
     product = Product.find params[:product_id]
     @line_item = @cart.line_items.build product: product
 
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+    @counter = session[:counter]
     @products = Product.order(:title)
   end
 end
