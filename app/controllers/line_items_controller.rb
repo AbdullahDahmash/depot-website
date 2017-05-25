a6679adb5c8101aed519dc26a5353a1e6a39a2ce
Abdullah <Abdullah.dahmash@yahoo.com>
2017-05-25 20:09:40 +0300

    added qunaitity to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
   def create
     session.delete :counter
     product = Product.find params[:product_id]
-    @line_item = @cart.line_items.build product: product
+    @line_item = @cart.add_product product
 
     respond_to do |format|
       if @line_item.save
